ad662e8755d4b401a575f8a71fd1009ffbd12fd0
h15200 <45977354+h15200@users.noreply.github.com>
2019-12-11 17:38:12 -0800

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
-import React from "react"
+import React, { Fragment } from 'react'
+import { Helmet } from 'react-helmet'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Fragment>
+      <Helmet>
+        <title>css practice with tutorial on udemy</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
+      </Helmet>
+    </Fragment>
+  )
+}
+
+export default IndexPage
